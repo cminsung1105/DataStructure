4ba957cc28ffd7a61635debaaf6f6dd9242fed32
Min Sung Cha <44740344+cminsung1105@users.noreply.github.com>
2018-11-05 11:35:31 -0800

    Update expmain.cpp

--- DataStructures/exp/expmain.cpp
+++ DataStructures/exp/expmain.cpp
@@ -1,11 +1,3 @@
-// expmain.cpp
-//
-// ICS 46 Spring 2018
-// Project #4: Set the Controls for the Heart of the Sun
-//
-// Do whatever you'd like here.  This is intended to allow you to experiment
-// with your code, outside of the context of the broader program or Google
-// Test.
 
 #include <algorithm>
 #include <iostream>
